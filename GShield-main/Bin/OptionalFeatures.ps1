4d4cd8e21041acb28763d2ee6eb3234efb70b7f3
GoranStambuk <znastidobrostoje@gmail.com>
2025-02-24 20:36:03 +0100

    Delete GShield-main/Bin directory

--- GShield-main/Bin/OptionalFeatures.ps1
+++
@@ -1,1 +0,0 @@
-Get-WindowsOptionalFeature -Online | Where-Object {$_.State -eq "Enabled"} | ForEach-Object {Disable-WindowsOptionalFeature -FeatureName $_.FeatureName -Online -NoRestart -Confirm:$false}
